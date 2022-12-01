9773fb73d286c0edbf3b41a67de7874f9a97a93a
Marija Jezeršek <marija.jezersek.02@gmail.com>
2022-12-01 22:46:59 +0100

    1. dan rešeno

--- src/solvers/year_2022/day1.ml
+++ src/solvers/year_2022/day1.ml
@@ -1,22 +1,29 @@
+(* #use "topfind";;
+   #require "str";; *)
 open Solvers.Signature
 open Utils.List_utils
 open Str
 
-(* module Solver : Solver = struct *)
-module Solver = struct
-  let rec vsota sez = match sez with [] -> 0 | h :: t -> h + vsota t;;
-  let max_number_list lst = List.fold_left max 0 lst;;
+module Solver : Solver = struct
+  let rec vsota sez = match sez with [] -> 0 | h :: t -> h + vsota t
+  let max_number_list lst = List.fold_left max 0 lst
 
-  let naloga1 data =
-    let lines = Str.split (regexp "\n \n") data in lines 
-      |> List.map String.trim
-      |> List.map (String.split_on_char '\n')
-      |> List.map (List.map int_of_string)
-      |> List.map vsota 
-      |> max_number_list
-      |> string_of_int
+  let naloga1 (data : string) =
+    data
+    |> Str.split (regexp "\n\n")
+    |> List.map String.trim
+    |> List.map (String.split_on_char '\n')
+    |> List.map (List.map int_of_string)
+    |> List.map vsota |> max_number_list |> string_of_int
 
-  let naloga2 (*data _part1*) = failwith "Dopolni me"
-  (* data |> List.lines |> List.int_list |> List.map full_cost |> List.sum
-     |> string_of_int *)
+  (* join two strings *)
+  let naloga2 (data : string) (_ : string) =
+    data
+    |> Str.split (regexp "\n\n")
+    |> List.map String.trim
+    |> List.map (String.split_on_char '\n')
+    |> List.map (List.map int_of_string)
+    |> List.map vsota
+    |> List.sort (fun a b -> b - a)
+    |> List.take 3 |> vsota |> string_of_int
 end
